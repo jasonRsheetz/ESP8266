69b5d85743b7dee2a69a976cc14e9c720865cc69
Jason <10270672@my.uvu.edu>
2018-04-07 14:07:00 -0600

    added function prologues for constructor, parameterized constructor,  and function to change wifi mode. also added class variables and pointer to softwareserail object. All changes untested at this point

--- ESP8266.h
+++ ESP8266.h
@@ -1,10 +1,28 @@
+#include <WString.h>
+#include <SoftwareSerial.h>
 
 class ESP8266
 {
 public:
 ESP8266();
+ESP8266(String, String, String, String, int, int);
+void Sleep(int);
+String Transmit(String);
+String SetWifiMode(int);
 
 private:
+//pointer to a software serial object
+SoftwareSerial *uart;
 
+//wifi varibles
+String SSID;
+String password;
 
+//softwareSerial pins
+int RX;
+int TX;
+
+//thingspeak variables
+String API_key;
+String channel_ID;
 };
