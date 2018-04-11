43276113a494f64fc1c1478a85ba9737e6afdf3f
Jason <10270672@my.uvu.edu>
2018-04-11 11:01:10 -0600

    Transmit function has been changed to only update one field at a time, the argument list has changed to allow passing a single data value the field it should be uploaded to , this has not been tested yet. A function to deep-sleep the module was added, the funtion has not been tested, however, sleep times of up to 30 minutes have been tested via serial port and worked fine.

--- ESP8266.cpp
+++ ESP8266.cpp
@@ -9,11 +9,8 @@ ESP8266::ESP8266()
 //Purpose: to set software serial pins to be used by the wifi module and set the ssid, password, API keys, and channel ID
 //parameters:4 Strings, 2 integers
 //return value(s): none
-ESP8266::ESP8266(String _SSID, String _password, String _API_key, String _Channel_ID, int _RX, int _TX)
+ESP8266::ESP8266(String _API_key, String _Channel_ID, int _RX, int _TX)
 {
-//set variables
-SSID = _SSID;
-password = _password;
 
 RX = _RX;
 TX = _TX;
@@ -58,10 +55,79 @@ while(uart->available() > 0)
 return returnString;
 }
 
+String ESP8266::JoinNetwork(String _SSID, String _password)
+{
+//set variables
+SSID = _SSID;
+password = _password;
+String returnString;
+
+String StringToSend = "AT+CWJAP_DEF=";
+StringToSend += SSID;
+StringToSend += ",";
+StringToSend += password;
 
 
+//wait for a response
+while(uart->available() == 0)
+{}
+
+//listen for the response and return it
+while(uart->available() > 0)
+{
+	if(uart->available())
+	{
+		returnString += char(uart->read());
+	}
+}
+
+return returnString;
+}
 
 
+String ESP8266::Transmit(int _data, String field)
+{
+String responseString;
+
+//establish connection with thingspeak.com
+uart->println("AT+CIPSTART=\"TCP\",\"184.106.153.149\",80");
+_delay_ms(3000);
+
+//the length of the web address for thingspeak 40 characters, plus to for the newline and carriage return characters
+uint8_t webAddressLength = 42;
+
+//find the length of the datafor O3
+String dataToString = String(_data);
+
+
+uint8_t dataLength =dataToString.length()+webAddressLength;
+
+//send the number of characters to be updated
+String dataLengthToString = String(dataLength);
+String dataLengthcmd = "AT+CIPSEND=";
+dataLengthcmd += dataLengthToString;
+
+//send the data length
+uart->println(dataLengthcmd);
+_delay_ms(3000);
+
+//piece the data together
+String dataToSend = "GET /update?key=E2TKCCBA49LSZK2Q&field" + field + "=" + dataToString;
+
+
+//close communications
+uart->println(dataToSend);
+_delay_ms(500);
+uart->println("AT+CIPCLOSE");
+
+return responseString;
+}
+
+void ESP8266::Sleep(int _sleepTime)
+{
+//transmit the sleep time
+uart->println(String(_sleepTime);
+}
 
 
 
